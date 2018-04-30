04d6698adad688ed7baac3b8187bf09f7af56900
mrap94 <perezarturo94@gmail.com>
2018-04-30 01:13:25 +0000

    Changed to https for heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var http = require('http');
+var http = require('https'); //http was original
 var url = require('url');
 
-http.createServer(function (req, res) {
-  req.setTimeout(0);
+var server = http.createServer(function (req, res) {
   const { headers, method, url } = req;
   res.writeHead(200, {'Content-Type': 'text/html'});
   console.log(url);
@@ -29,3 +28,5 @@ http.createServer(function (req, res) {
     }
   }
 }).listen(process.env.PORT || 5000);
+
+//server.timeout= 10000;
