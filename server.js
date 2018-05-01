ee981f6d37baf584867090cf5e8eef85d436bdb3
mrap94 <perezarturo94@gmail.com>
2018-05-01 19:24:40 +0000

    Fix undefined map

--- server.js
+++ server.js
@@ -54,17 +54,10 @@ if (cluster.isMaster) {
           output: 'json',
           width: '500'
         }).then((queryresult) => {
-            const pods = queryresult.pods;
-            const output = pods.map((pod) => {
-            const subpodContent = pod.subpods.map(subpod =>
-            `  <img src="${subpod.img.src}" alt="${subpod.img.alt}">`
-         ).join('\n');
-            return `<h2>${pod.title}</h2>\n${subpodContent}`;
-         }).join('\n');
-            console.log(output);
-        }).catch(console.error); 
-      }
+          console.log(queryresult.pods[0].subpods[0].plaintext)
+      }).catch(console.error) 
     }
+  }
 }).listen(process.env.PORT || 5000);
 
 server.timeout= 30000;
