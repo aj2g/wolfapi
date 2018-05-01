78f38f0a833a25b76bbba4f83532a72da34e83be
mrap94 <perezarturo94@gmail.com>
2018-05-01 18:50:36 +0000

    Change display to webpage

--- server.js
+++ server.js
@@ -53,7 +53,16 @@ if (cluster.isMaster) {
           format: 'mathml',
           output: 'json',
           width: '500'
-        }).then(console.log, console.error);   
+        }).then((queryresult) => {
+            const pods = queryresult.pods;
+            const output = pods.map((pod) => {
+            const subpodContent = pod.subpods.map(subpod =>
+            `  <img src="${subpod.img.src}" alt="${subpod.img.alt}">`
+         ).join('\n');
+            return `<h2>${pod.title}</h2>\n${subpodContent}`;
+         }).join('\n');
+            console.log(output);
+        }).catch(console.error); 
       }
     }
 }).listen(process.env.PORT || 5000);
