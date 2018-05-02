bd53f3922a21c402e7d189b05ce90e38d67f358d
mrap94 <perezarturo94@gmail.com>
2018-05-02 00:28:04 +0000

    test

--- server.js
+++ server.js
@@ -47,11 +47,18 @@ function API(){
           includepodid: 'Result',
           podstate: 'Step-by-step',
           //appid: waApi,
-          format: 'plaintext',  // change back to image
+          format: 'image',  // change back to image
           //output: 'json',
         }).then((queryresult) => {
-            res.end('<script type="text/javascript" src="ASCIIMathML.js"></script>' + queryresult.pods[0].subpods[2].plaintext)
-        }).catch(console.error)
+          const pods = queryresult.pods;
+          const output = pods.map((pod) => {
+          const subpodContent = pod.subpods.map(subpod =>
+          `  <img src="${subpod.img.src}" alt="${subpod.img.alt}">`
+        ).join('\n');
+          return `<h2>${pod.title}</h2>\n${subpodContent}`;
+        }).join('\n');
+          res.end(output);
+        }).catch(console.error);
     }
   }
 }).listen(process.env.PORT || 5000);
